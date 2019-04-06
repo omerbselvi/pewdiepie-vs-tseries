ddda738d217c8ae8a27aaff12e9e5098450e795a
omerbselvi <omerbugra97@hotmail.com>
2019-04-07 02:40:37 +0300

    update background on render

--- main.py
+++ main.py
@@ -3,12 +3,14 @@ import os.path
 import matplotlib.pyplot as plt
 import matplotlib
 import matplotlib.animation as animation
+import random
 from pygame import mixer
-
 
 my_path = os.path.abspath(os.path.dirname(__file__))
 url = "https://www.googleapis.com/youtube/v3/channels?part=statistics&forUsername={}&key={}"
 key = "*INSERT API KEY HERE*"
+colors = ['tab:blue', 'tab:orange', 'tab:green', 'tab:red', 'tab:purple', 'tab:brown', 'tab:pink',
+          'tab:gray', 'tab:olive', 'tab:cyan']
 
 fig = plt.figure(num=None, figsize=(10, 8), dpi=80, facecolor='w', edgecolor='k')
 ax1 = fig.add_subplot(2, 1, 1)
@@ -23,13 +25,11 @@ def get_subs_count(channel_id):
     r_url = url.format(channel_id, key)
     req = requests.get(r_url).json()
     subs_count = req['items'][0]["statistics"]["subscriberCount"]
-    print(channel_id, " :", subs_count)
     return int(subs_count)
 
 
 def playBL():
     path = my_path + "/res/bitch-lasagna.ogg"
-    print(path)
     mixer.init()
     mixer.music.load(path)
     mixer.music.play(-1)
@@ -53,22 +53,27 @@ def animate(i):
     ax1.ticklabel_format(useOffset=False)
     ax1.get_yaxis().set_major_formatter(
         matplotlib.ticker.FuncFormatter(lambda x, p: format(int(x), ',')))
-    ax1.plot(xar, yar, '-o')
+    ax1.plot(xar, yar, '-ob')
     ax2.clear()
     ax2.set_title("T-Series")
     ax2.ticklabel_format(useOffset=False)
     ax2.get_yaxis().set_major_formatter(
         matplotlib.ticker.FuncFormatter(lambda x, p: format(int(x), ',')))
-    ax2.plot(x2ar, y2ar, '-or')
+    ax2.plot(x2ar, y2ar, '-ob')
     ax2.set_facecolor('xkcd:salmon')
     ax1.set_ylabel('Pewdiepie Sub Count')
     ax2.set_ylabel('T-Series Sub Count')
     plt.xlabel('Time(Seconds)')
     fig.canvas.set_window_title('Pewdiepie vs T-Series')
-    print(yar)
-    print(xar)
+    updateOnRender()
+
+
+def updateOnRender():
+    ax1.set_facecolor(random.choice(colors))
+    ax2.set_facecolor(random.choice(colors))
+    fig.set_facecolor(random.choice(colors))
 
 
 playBL()
-ani = animation.FuncAnimation(fig, animate, interval=3000)
+ani = animation.FuncAnimation(fig, animate, interval=500)
 plt.show()
